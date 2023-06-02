2ee5b9b76d25ee79059e72c4e229e5a92c0feaf5
GOPIKRISHNA S <gk120000ks@gmail.com>
2023-06-02 20:37:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
-// Middleware to parse JSON request bodies
+
 app.use(express.json());
 
 // Read the hospital data from the JSON file
@@ -16,37 +16,37 @@ fs.readFile('data.json', 'utf8', (err, data) => {
 
   let hospitalData = JSON.parse(data);
 
-  // GET request handler
-  app.get('/', (req, res) => {
+ 
+  app.get('/get1/', (req, res) => {
     res.json(hospitalData);
   });
 
-  // POST request handler
-  app.post('/', (req, res) => {
+ 
+  app.post('/post1/', (req, res) => {
     const newHospital = req.body;
     hospitalData.push(newHospital);
     fs.writeFileSync('data.json', JSON.stringify(hospitalData));
     res.status(201).send('Hospital data added successfully');
   });
 
-  // PUT request handler
+
   app.put('/update/:id', (req, res) => {
     const hospitalId = req.params.id;
     const updatedHospital = req.body;
     hospitalData[hospitalId] = updatedHospital;
     fs.writeFileSync('data.json', JSON.stringify(hospitalData));
     res.send('Hospital data updated successfully');
-  });
+  }); //array starts from 0
 
-  // DELETE request handler
+  
   app.delete('/delete/:id', (req, res) => {
     const hospitalId = req.params.id;
     hospitalData.splice(hospitalId, 1);
     fs.writeFileSync('data.json', JSON.stringify(hospitalData));
     res.send('Hospital data deleted successfully');
-  });
+  }); //array starts from 0
 
-  // Start the server
+  
   app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
   });
